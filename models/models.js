71b3c7b71040fa6299799881739e4a1ee4bbaba4
Jorge Anta <jorgeanta2@gmail.com>
2015-07-27 20:29:34 +0200

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,22 +1,46 @@
-var path= require('path');
+var path = require('path');
 
-var Sequelize=require('sequelize');
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 
-var sequelize=new Sequelize(null,null,null,
-						{dialect:"sqlite",storage:"quiz.sqlite"}
-						);
-var Quiz=sequelize.import(path.join(__dirname,'quiz'));
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
 
-exports.Quiz=Quiz;
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
+);
 
-sequelize.sync().success(function(){
-	Quiz.count().success(function(count){
-		if(count===0){
-			Quiz.create({
-				pregunta:'Capital de Italia',
-				respuesta: 'Roma'
-			})
-			.success(function(){console.log('Base de datos inicializada')});
-		};
-	});
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+exports.Quiz = Quiz; // exportar tabla Quiz
+
+// sequelize.sync() inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
+      Quiz.create({ pregunta: 'Capital de Italia',
+      	            respuesta: 'Roma'
+      	         })
+      .then(function(){console.log('Base de datos inicializada')});
+    };
+  });
 });
