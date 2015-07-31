534670819cca093efa5d9722da7463bf5492e0a0
Jorge Anta <jorgeanta2@gmail.com>
2015-07-31 19:05:26 +0200

    Practica Tema 7 Buscador

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,24 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
+  if(req.query.search){
+    var search=req.query.search;
+    search=search.replace(" ","%");
+    search="%"+search+"%";
+    console.log(search);
+    models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes) {
+    res.render("quizes/index", {quizes:quizes});
+    }).catch(function(error) { next(error);});
+  }
+  else
+  {
+    models.Quiz.findAll().then(
     function(quizes) {
       res.render('quizes/index', { quizes: quizes});
     }
-  ).catch(function(error) { next(error);})
+    ).catch(function(error) { next(error);})
+  }
+
 };
 
 // GET /quizes/:id
