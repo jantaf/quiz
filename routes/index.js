4b1063b34dcdb894bed31ed1ea4c57bd083aec8a
Jorge Anta <jorgeanta2@gmail.com>
2015-08-03 12:34:39 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var router = express.Router();
 
@@ -16,5 +15,7 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 
 module.exports = router;
